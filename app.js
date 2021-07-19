605de3ad3d2d96b70632985322d344a0916c2252
proraj9088 <rp8735685@gmail.com>
2021-07-19 10:52:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+
+
+
+
+
+
+
+app.listen(3003,()=> {
+     console.log("server running at port 3003");
+})
